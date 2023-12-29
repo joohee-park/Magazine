549a2adcd5f4272d117cab5184cd00950a422b0f
jooheepark <goinguptheshore@gmail.com>
2023-12-29 11:35:17 +0900

    readme수정

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './css/myreset.css'
-import './css/common.css'
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./css/myreset.css";
+import "./css/common.css";
+import { BrowserRouter } from "react-router-dom";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
